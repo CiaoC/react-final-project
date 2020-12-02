ee597fcba4cb9168928a63ddd56531120c75ab81
CiaoC <70588807+CiaoC@users.noreply.github.com>
2020-12-02 11:41:32 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">
+  return (
+    <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
   <footer>
       This project was coded by Cindy Chau and is {" "}
   <a href="https://github.com/CiaoC/react-final-project" target="_blank"> open-sourced on GitHub</a>
   </footer>
-  </div>;   
+  </div>
+  </div>  
+);
 }
 
 
