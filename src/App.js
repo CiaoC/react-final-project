60bad52d123bc17e0ecea166412de55e56e600a9
CiaoC <70588807+CiaoC@users.noreply.github.com>
2020-12-02 11:17:09 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { waitForElementToBeRemoved } from "@testing-library/react";
 import React from "react";
 import './App.css';
 
-function App() {
-  return <div className="App">Weather App</div>;   
+export default function App() {
+  return <div className="App">Weather App
+  <footer>
+      This project was coded by Cindy Chau and is 
+  <a href="https://github.com/CiaoC/react-final-project" target="_blank">open-sourced on GitHub</a>
+  </footer>
+  </div>;   
 }
 
-export default App;
+
