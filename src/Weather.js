20379190c7930db1beacd754340c7f02da955a6e
CiaoC <70588807+CiaoC@users.noreply.github.com>
2020-12-06 13:18:59 -0700

    Added weather icons from axios
    via openweather API

--- src/Weather.js
+++ src/Weather.js
@@ -13,15 +13,14 @@ export default function Weather(props) {
             ready: true,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
-            date: new Date (response.data.dt*1000),
+            date: new Date(response.data.dt * 1000),
             wind: response.data.wind.speed,
-            city: response.data.name, 
+            city: response.data.name,
             precipitation: response.data.clouds.all,
             description: response.data.weather[0].description,
-            icon: "https://s1.twnmm.com/images/en_ca/icons/wxicons_large/2.png"
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         });
     }
-
     function search() {
         let apiKey = "9c48a62dcc12a129cf6c63c31fa92ac6";
         let apiUrl =
@@ -40,10 +39,10 @@ export default function Weather(props) {
 
     if (weatherData.ready) {
         return (
-        <div className="Weather">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-9">
+            <div className="Weather">
+                <form onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="col-9">
                             <input
                                 type="search"
                                 placeholder="Enter a City..."
@@ -51,17 +50,17 @@ export default function Weather(props) {
                                 autoFocus="on"
                                 onChange={handleCityChange}
                             />
+                        </div>
+                        <div className="col-3">
+                            <input type="submit" value="Search" className="btn btn-info w-100" /></div>
                     </div>
-                    <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-info w-100"/></div>
-               </div>     
-            </form>
-                <WeatherStat data={weatherData} /> 
-        </div>
-    );
+                </form>
+                <WeatherStat data={weatherData} />
+            </div>
+        );
     } else {
         search();
         return "Fetching data...";
     
-    }  
+    }
 }
