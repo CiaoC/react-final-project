75ae402ad8d4f09883a4effc33c20225e4936a4c
CiaoC <70588807+CiaoC@users.noreply.github.com>
2020-12-08 13:25:22 -0700

    Improved styling

--- src/WeatherStat.js
+++ src/WeatherStat.js
@@ -28,11 +28,13 @@ export default function WeatherStat(props) {
                     </div>
                 </div>
                 <div className="col-6">
+                      
                     <ul>
                         <li> Humidity: {props.data.humidity}% </li>
                         <li> Wind: {Math.round(props.data.wind)} km/h </li>
                         
-                    </ul>
+                            </ul>
+                       
                 </div>
             </div>
         </div>
